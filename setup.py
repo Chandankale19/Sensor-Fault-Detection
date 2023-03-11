0081231abf3494a1db529f252afd4d19e9f138da
chandankale19 <chandankale1997@gmail.com>
2023-03-11 07:12:48 +0530

    update

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="Sensor Data Prediction For - Silicon Wafer Fault Detection.",
+    version="0.0.1",
+    description="CDAC PG-DBDA ML Project F-Group",
+    author="Amitava Gosh", 
+    packages=find_packages(),
+    license="Apache License"
+)
